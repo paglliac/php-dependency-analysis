1f3d027452d654e340375e7adbe8c8dcf0ee502b
Kir Apukhtin <paglliac@gmail.com>
2021-07-16 17:57:20 +0700

    [Refactoring] Move stub project to Data directory for shared access from all kind of tests

--- tests/Functional/RunAnalysisTest.php
+++ tests/Functional/RunAnalysisTest.php
@@ -11,7 +11,7 @@ class RunAnalysisTest extends TestCase
     public function testAnalyzerRun()
     {
         $facade = new AnalyzerFacade();
-        $result = $facade->run(__DIR__ . '/simpleProject/config.php');
+        $result = $facade->run(__DIR__ . '../Data/simpleProject/config.php');
         $this->assertTrue($result, 'Result of analysis simpleProject should be true');
     }
 
