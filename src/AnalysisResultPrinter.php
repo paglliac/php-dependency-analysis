21e7c00e4f8e9fe1d75b5825251a3a6089569c19
Kir Apukhtin <paglliac@gmail.com>
2021-07-20 19:33:37 +0700

    [Feature] Add result printer

---
+++ src/AnalysisResultPrinter.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace DependencyAnalysis;
+
+
+class AnalysisResultPrinter
+{
+    public function print(AnalysisResult $analysisResult)
+    {
+        if ($analysisResult->isSuccess()) {
+            fwrite(
+                STDOUT,
+                PHP_EOL .
+                'All dependencies inside your project satisfy the approved dependency graph' . PHP_EOL
+            );
+        } else {
+            fwrite(
+                STDOUT,
+                PHP_EOL .
+                'You have problems with dependencies in your project' . PHP_EOL
+            );
+        }
+    }
+
+}
