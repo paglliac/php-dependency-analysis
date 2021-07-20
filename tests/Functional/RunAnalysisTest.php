71a083e072238f303caa4c6ede046976a392db58
Kir Apukhtin <paglliac@gmail.com>
2021-07-20 19:24:33 +0700

    [Fix] Add test for command run and fix error with wrong error code

--- tests/Functional/RunAnalysisTest.php
+++ tests/Functional/RunAnalysisTest.php
@@ -16,4 +16,13 @@ class RunAnalysisTest extends TestCase
         $this->assertEquals(4, $result->analyzedFilesAmount());
     }
 
+    public function testRunCommand()
+    {
+        $retval = null;
+        $output = null;
+
+        exec(__DIR__ . '/../../bin/php-da -c ' . __DIR__ . '/../Data/simpleProject/config.php', $output, $retval);
+
+        $this->assertEquals(0, $retval);
+    }
 }
