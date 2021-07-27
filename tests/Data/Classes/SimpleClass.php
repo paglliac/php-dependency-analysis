338ef8a0ca7bd96b1072314b5688d1b57ad582f9
Kir Apukhtin <paglliac@gmail.com>
2021-07-27 19:51:48 +0700

    [Fix] Fix broken test after IDE optimize imports

--- tests/Data/Classes/SimpleClass.php
+++ tests/Data/Classes/SimpleClass.php
@@ -1,13 +1,16 @@
 <?php
 /** @noinspection PhpUnused */
-
 /** @noinspection PhpUnusedAliasInspection */
 
 
 namespace DependencyAnalysis\Tests\Data\Classes;
 
 
+use Domain\Cargo;
+
+
 class SimpleClass
 {
+    private Cargo $cargo;
 
 }
